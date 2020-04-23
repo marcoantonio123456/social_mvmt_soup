ff9a39a696dcd386f6b8172d888760fac7827618
Marco Antonio <54124842+marcoantonio123456@users.noreply.github.com>
2020-04-23 15:01:02 -0400

    .
    .

--- mvmd_soup.py
+++ mvmd_soup.py
@@ -1,11 +1,16 @@
 
 # MY SUGGESTED LIST
-# mvmt=["#vegan", "#restaurant", "#queenwest", "#westqueenwest", "#toreats", "#bonapetite", "#decor", "#veganbreakfast", "#veganlove", "#animallover", "#veganshare", "#animallove", "#glutenfree", "#organic", "#veganbeauty",
+
+# mvmt=["#vegan", "#restaurant", "#queenwest", "#westqueenwest", 
+# "#toreats", "#bonapetite", "#decor", "#veganbreakfast", "#veganlove", 
+#"#animallover", "#veganshare", "#animallove", "#glutenfree", "#organic", "#veganbeauty",]
 
 # BELOW ARE HASHTAGS TO PICK FROM. THE LONGER THE LIST THE MORE WE GET TO PICK FROM
+
 mvmt = ["#sustainability", "#sustainable", "#blogto", "#torontoeats", 
 "#parkdale" ,"#queenwest" ,"#libertyvillage", "#vegan" ,"#foodie" ,
 "#torontofoodie", "#health", "#blogger", "#travel", "#takeout"]
+
 # OUR UNIQUE TAGS THAT WILL SHOW FIRST
 base=["#relaxedfoods", "#restaurant", "#toronto", ]
 
@@ -18,4 +23,7 @@ def listToString(totalsoup):
     str1 = " " 
     return (str1.join(totalsoup)) 
 print(listToString(totalsoup))
+
+
+
 
